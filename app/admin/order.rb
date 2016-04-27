0f99cabc3ee7c78f263edefb50dda0ae60ae1b70
ejakait <ejakaitcaleb@outlook.com>
2016-04-27 22:05:13 +0300

    orders resource changes

--- app/admin/order.rb
+++ app/admin/order.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Order do
-
+    # actions :all, :except => [:new, :create]
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
--- db/migrate/20160427174957_create_orders.rb
+++ db/migrate/20160427174957_create_orders.rb
@@ -1,8 +1,10 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
+      t.integer :user_id
 
       t.timestamps null: false
     end
+    add_index :orders , :user
   end
 end
