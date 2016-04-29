0998b070b926abbc3ffd3e6024a5426ec5829d1c
Emmanuel Aghan <aghan.odhiambo@strathmore.edu>
2016-04-29 09:12:13 +0300

    ib

--- db/schema.rb
+++ db/schema.rb
@@ -46,14 +46,29 @@ ActiveRecord::Schema.define(version: 20160428174017) do
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
 
+  create_table "admins", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+  end
+
+  add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
+  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
+
   create_table "categories", force: :cascade do |t|
     t.string   "name",        limit: 255
     t.text     "description", limit: 65535
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
-  end
-
-  create_table "categories_ebooks_joins", force: :cascade do |t|
   end
 
   create_table "categorizations", force: :cascade do |t|
@@ -64,7 +79,7 @@ ActiveRecord::Schema.define(version: 20160428174017) do
   end
 
   create_table "ebook_catrgories", id: false, force: :cascade do |t|
-    t.integer "category_id", limit: 4
+    t.string  "category_id", limit: 255
     t.integer "ebook_id",    limit: 4
   end
 
@@ -76,13 +91,15 @@ ActiveRecord::Schema.define(version: 20160428174017) do
     t.string   "pdf",         limit: 255
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
-    t.integer  "category_id", limit: 4
   end
 
   create_table "orders", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
   end
+
+  add_index "orders", ["user_id"], name: "index_orders_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "provider",               limit: 255,   default: "email", null: false
