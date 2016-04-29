ed6fea17cd8f252986f111885556601c83213b21
ejakait <ejakaitcaleb@outlook.com>
2016-04-29 09:58:05 +0300

    adding batch actions to categories

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -1,15 +1,17 @@
 ActiveAdmin.register Category do
     permit_params :name, :description
-    
+
     #filter options
     filter :name
-    
+
     #columns displayed on Categories page
     index do
+    selectable_column
     column :name
     column :description
+    actions
   end
-    
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
