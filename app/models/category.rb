6c0e2ac1e705632c5d90f15db15e41c5be248bb9
ejakait <ejakaitcaleb@outlook.com>
2016-05-08 23:51:49 +0300

    merge resolve

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 class Category < ActiveRecord::Base
   has_many :ebooks, :dependent => :restrict_with_error
 end
-=======
-class Category < ActiveRecord::Base
-  has_many :ebooks
-end
->>>>>>> a65c98790070bd935e20ebb72a496469b07f943a
+
