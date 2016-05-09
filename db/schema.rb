847ad733f510cf30a59dc8eb137d4587fde62b5b
Angela Namikoye <anamikoye52@gmail.com>
2016-05-09 14:56:40 +0300

    gajd

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160502130959) do
+ActiveRecord::Schema.define(version: 20160427133238) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
@@ -46,31 +46,34 @@ ActiveRecord::Schema.define(version: 20160502130959) do
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
 
+  create_table "admins", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+  end
+
+  add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
+  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
+
   create_table "categories", force: :cascade do |t|
-    t.string   "name",        limit: 255,   null: false
+    t.string   "name",        limit: 255
     t.text     "description", limit: 65535
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
   end
 
-  create_table "categorizations", id: false, force: :cascade do |t|
-    t.integer  "category_id", limit: 4
-    t.integer  "ebook_id",    limit: 4
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
-  end
-
-  add_index "categorizations", ["category_id"], name: "index_categorizations_on_category_id", using: :btree
-  add_index "categorizations", ["ebook_id"], name: "index_categorizations_on_ebook_id", using: :btree
-
-  create_table "categorizatons", force: :cascade do |t|
-    t.integer  "category_id", limit: 4, null: false
-    t.integer  "ebook_id",    limit: 4, null: false
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
-  end
-
   create_table "ebooks", force: :cascade do |t|
+    t.integer  "category_id", limit: 4
+    t.string   "ebook_id",    limit: 255
     t.string   "title",       limit: 255
     t.string   "author",      limit: 255
     t.text     "description", limit: 65535
@@ -78,13 +81,9 @@ ActiveRecord::Schema.define(version: 20160502130959) do
     t.string   "pdf",         limit: 255
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
-    t.integer  "category_id", limit: 4
   end
 
-  create_table "orders", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+  add_index "ebooks", ["category_id"], name: "index_ebooks_on_category_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "provider",               limit: 255,   default: "email", null: false
