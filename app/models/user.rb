a9fb80c74bda83c1fb34881585eb091912f35bb8
ejakait <ejakaitcaleb@outlook.com>
2016-05-10 16:00:01 +0300

    adding lockable function to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
    devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable
+          :confirmable,:lockable
           # commented out:
           # :omniauthable
   include DeviseTokenAuth::Concerns::User
