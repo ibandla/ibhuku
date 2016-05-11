89892d3d8d9eab0c0a02362c6e424b9d20499649
Clive Makamara <cmosh@live.com>
2016-05-11 16:15:27 +0300

    Filter pt 2

--- app/assets/app/controllers/ProductController.js
+++ app/assets/app/controllers/ProductController.js
@@ -1,4 +1,15 @@
- app.controller('ProductCtrl', function($scope) {
+app.controller('ProductCtrl', function($scope,ngCart) {
+
+	$scope.checkcart = function(id) {
+ 		if(ngCart.getItemById(id)==false)
+ 			return "block";
+ 		else 
+ 			return "none";
+  };
+ 	
+     $scope.currencySymbol = 'KES';
+     $scope.blah = 'haha';
+	$scope.image = ['<%= path_to_asset("f1.jpg") %>','B'];
 
    $scope.hello = "Hello World!";
 
--- app/assets/app/controllers/ProductsController.js
+++ app/assets/app/controllers/ProductsController.js
@@ -1,4 +1,4 @@
- app.controller('ProductsCtrl', function($scope,Ebooks) {
+ app.controller('ProductsCtrl', function($scope,Ebook,ngCart) {
 
   	$scope.ebooks = Ebook.all();
 
@@ -6,6 +6,17 @@
       min: 0,
       max: 1000000
     }
+
+    $scope.checkcart = function(id) {
+ 		if(ngCart.getItemById(id)==false)
+ 			return "block";
+ 		else 
+ 			return "none";
+  };
+ 	
+     $scope.currencySymbol = 'KES';
+     $scope.blah = 'haha';
+	$scope.image = ['<%= path_to_asset("f1.jpg") %>','B'];
 
 
 });
