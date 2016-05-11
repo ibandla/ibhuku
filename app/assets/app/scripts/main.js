7bf88764e8876aa10d552352a3dda97afcf93b64
Clive Makamara <cmosh@live.com>
2016-05-11 13:38:18 +0300

    trim js

--- app/assets/app/scripts/main.js
+++ app/assets/app/scripts/main.js
@@ -6,13 +6,10 @@
 		window.scrollTo(0,1);
 	}
 
-$(document).ready(function(){
-	// $(".megamenu").megamenu();
-});
  $(document).ready(function () {
      $('#horizontalTab').easyResponsiveTabs({
-         type: 'horizontal', //Types: default, vertical, accordion
-         width: 'auto', //auto or any width like 600px
-         fit: true   // 100% fit in a container
+         type: 'horizontal', 
+         width: 'auto', 
+         fit: true   
      });
- });	
+ });		
