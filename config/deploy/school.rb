38a7cc32e974f4a1a125c65283dab1701844697f
Clive Makamara <cmosh@live.com>
2016-05-15 17:43:54 +0300

    deploy user

--- config/deploy/school.rb
+++ config/deploy/school.rb
@@ -28,7 +28,7 @@ else
   school_host = '41.89.6.209'
 end
 
-ssh_command = "ssh -p 13322 mnch@#{school_host} -W %h:%p"
+ssh_command = "ssh -p 13322 deploy@#{school_host} -W %h:%p"
 set :ssh_options, { forward_agent: true, user: 'cmakamara', proxy: Net::SSH::Proxy::Command.new(ssh_command)}
 
 ## Defaults:
