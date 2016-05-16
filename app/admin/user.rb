f942cb6f5aa4d09c522f9c676000157c7a110026
ejakait <ejakaitcaleb@outlook.com>
2016-05-16 17:08:00 +0300

    blocking users

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register User do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :banned
 #
 # or
 #
@@ -13,16 +13,29 @@ ActiveAdmin.register User do
 #   permitted
 # end
 
+  batch_action :banned do |ids|
+    User.find(ids).each do |user|
+      user.banned! :banned 
+    end
+    redirect_to collection_path, alert: "user(s) have been blocked."
+  end
+  actions :all, :except => [:new,:create,:destroy]
+  action_item :banned, only: :index do
+      link_to 'Block', root_path
+      end
 
-  actions :all, :except => [:new, :create]
-  index do
+
+  index do 
+  selectable_column
     column :name
     column :email
     actions
+    
+      
   end
   
   show do
-  attributes_table :name, :email
+  attributes_table :name, :email,:banned
   end
   
   filter :name
---
+++ db/migrate/20160516115543_add_state_to_users.rb
@@ -0,0 +1,5 @@
+class AddStateToUsers < ActiveRecord::Migration
+  def change
+      add_column :users, :banned, :boolean
+  end
+end
