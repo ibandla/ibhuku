0c00b8ad96279c1528188f008285ae98e3e0223d
Clive Makamara <cmosh@live.com>
2016-05-21 18:47:29 +0300

    sc

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516115543) do
+ActiveRecord::Schema.define(version: 20160518164709) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -102,6 +102,7 @@ ActiveRecord::Schema.define(version: 20160516115543) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "banned"
+    t.boolean  "blocked",                              default: false,   null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
