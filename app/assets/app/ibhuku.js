0e1c15a77353fa5fe06dff7919de167ff1fc65c0
Clive Makamara <cmosh@live.com>
2016-05-22 21:03:42 +0300

    asynchronous pitfall fix
    app loads before angular

--- app/assets/app/ibhuku.js
+++ app/assets/app/ibhuku.js
@@ -10,5 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require frontend
 //= require ./main
 //= require_tree .
