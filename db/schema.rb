97f75697b6dea707c74ba4e2d47f748b7cdf4a30
Emmanuel Aghan <aghan.odhiambo@strathmore.edu>
2016-06-18 20:54:16 +0300

    about us

--- db/schema.rb
+++ db/schema.rb
@@ -48,16 +48,6 @@ ActiveRecord::Schema.define(version: 20160618125750) do
     t.datetime "updated_at",                null: false
   end
 
-  create_table "categorisations", force: :cascade do |t|
-    t.integer  "ebook_id",    limit: 4
-    t.integer  "category_id", limit: 4
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
-  end
-
-  add_index "categorisations", ["category_id"], name: "index_categorisations_on_category_id", using: :btree
-  add_index "categorisations", ["ebook_id"], name: "index_categorisations_on_ebook_id", using: :btree
-
   create_table "categorizations", force: :cascade do |t|
     t.integer  "category_id", limit: 4
     t.integer  "ebook_id",    limit: 4
@@ -86,7 +76,8 @@ ActiveRecord::Schema.define(version: 20160618125750) do
   add_index "ebook_orders", ["order_id"], name: "index_ebook_orders_on_order_id", using: :btree
 
   create_table "ebooks", force: :cascade do |t|
-    t.string   "ISBN",        limit: 255
+    t.integer  "int_id",      limit: 4
+    t.string   "ebook_id",    limit: 255
     t.string   "title",       limit: 255
     t.string   "author",      limit: 255
     t.text     "description", limit: 65535
@@ -96,6 +87,8 @@ ActiveRecord::Schema.define(version: 20160618125750) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
   end
+
+  add_index "ebooks", ["int_id"], name: "index_ebooks_on_int_id", using: :btree
 
   create_table "executes", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -148,12 +141,13 @@ ActiveRecord::Schema.define(version: 20160618125750) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email",      limit: 255
-    t.string   "fullname",               limit: 255
+    t.string   "name",                   limit: 255
     t.integer  "title_id",               limit: 4
     t.string   "email",                  limit: 255
     t.text     "tokens",                 limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "banned"
     t.boolean  "blocked",                              default: false,   null: false
     t.boolean  "resblock",                             default: false,   null: false
   end
@@ -163,10 +157,6 @@ ActiveRecord::Schema.define(version: 20160618125750) do
   add_index "users", ["title_id"], name: "fk_rails_e9277efd4e", using: :btree
   add_index "users", ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
 
-  add_foreign_key "cart_items", "ebooks"
-  add_foreign_key "cart_items", "users"
-  add_foreign_key "categorisations", "categories"
-  add_foreign_key "categorisations", "ebooks"
   add_foreign_key "categorizations", "categories"
   add_foreign_key "categorizations", "ebooks"
   add_foreign_key "ebook_orders", "ebooks"
