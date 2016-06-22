1ecea6f0f477b8456e1ebebe6cbee7bfca851566
Clive Makamara <cmosh@live.com>
2016-06-22 11:21:53 +0300

    puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,9 +1,13 @@
 unless ENV['RAILS_ENV'] == 'production'
  require 'localtunnel'
-
- Localtunnel::Client.start(
-  port: 3000,
-  subdomain: 'ibhuku'
-)
- puts Localtunnel::Client.url
+	begin
+		
+	 Localtunnel::Client.start(
+	  port: 3000,
+	  subdomain: 'ibhuku'
+	)
+	 puts Localtunnel::Client.url
+	rescue Exception => e
+		puts "Unable to create public url"
+	end
 end
