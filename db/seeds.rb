725c7921afb7dc34417b45c446d306cc176e11eb
ejakait <ejakaitcaleb@outlook.com>
2016-06-27 00:52:48 +0300

    deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 
 Category.create!(name:'Fiction', description:"1")
 Category.create!(name: 'Classic', description:"1")
