3bf5aaa651cc6651411f7e5250388ca01e1dc3e3
[anamikoye] <[anamikoye52@gmail.com]>
2016-06-26 21:15:19 +0300

    Move to Home

--- app/assets/app/controllers/EbooksController.js
+++ app/assets/app/controllers/EbooksController.js
@@ -40,51 +40,6 @@
     min: 0,
     max: 1500
   }
-
-  var docDefinition = {
-    content: [
-      {
-        text: 'IBHUKU CATALOG', style: 'header'
-      },
-      {
-        style: 'demoTable',
-        table: {
-          widths: ['*', '*', '*','*','*'],
-          body: [
-            [{text: 'Title', style: 'header'}, {text: 'Author', style: 'header'}, {text: 'Category', style: 'header'},
-              {text: 'ISBN', style: 'header'},{text: 'Price', style: 'header'}
-            ],
-            
-            ]
-        },
-        layout: 'lightHorizontalLines'
-      }
-    ],
-    styles: {
-      header: {
-        bold: true,
-        color: '#000',
-        fontSize: 11,
-        alignment: 'center'
-      },
-      demoTable: {
-        color: '#666',
-        fontSize: 10
-      }
-    }
-  };
-
-  $scope.openPdf = function() {
-      pdfMake.createPdf(docDefinition).open();
-    };
-
-  $scope.downloadPdf = function() {
-      pdfMake.createPdf(docDefinition).download();
-    };
-
-
-
-
     // $scope.searchParams = {
     //   "query":""
     // };
--- app/assets/app/controllers/HomeController.js
+++ app/assets/app/controllers/HomeController.js
@@ -7,4 +7,84 @@
     // $scope.test = Restangular.all('cart').getList().$object;
 
     $scope.test = $rootScope.user;
+
+    // var docDefinition = {
+    //   content: [
+    //     {
+    //       text: 'IBHUKU CATALOG', style: 'header'
+    //     },
+    //     {
+    //       style: 'demoTable',
+    //       table: {
+    //         widths: ['*', '*', '*','*','*'],
+    //         body: [
+    //           [{text: 'Title', style: 'header'}, {text: 'Author', style: 'header'}, {text: 'Category', style: 'header'},
+    //             {text: 'ISBN', style: 'header'},{text: 'Price', style: 'header'}
+    //           ],
+    //
+    //           ]
+    //       },
+    //       layout: 'lightHorizontalLines'
+    //     }
+    //   ],
+    //   styles: {
+    //     header: {
+    //       bold: true,
+    //       color: '#000',
+    //       fontSize: 11,
+    //       alignment: 'center'
+    //     },
+    //     demoTable: {
+    //       color: '#666',
+    //       fontSize: 10
+    //     }
+    //   }
+    // };
+
+    var externalDataRetrievedFromServer = [
+    { name: 'Bartek', age: 34 },
+    { name: 'John', age: 27 },
+    { name: 'Elizabeth', age: 30 },
+];
+
+    function buildTableBody(data, columns) {
+    var body = [];
+
+    body.push(columns);
+
+    data.forEach(function(row) {
+        var dataRow = [];
+
+        columns.forEach(function(column) {
+            dataRow.push(row[column].toString());
+        })
+
+        body.push(dataRow);
+    });
+
+    return body;
+}
+
+function table(data, columns) {
+    return {
+        table: {
+            headerRows: 1,
+            body: buildTableBody(data, columns)
+        }
+    };
+}
+
+var dd = {
+    content: [
+        { text: 'Dynamic parts', style: 'header' },
+        table(externalDataRetrievedFromServer, ['name', 'age'])
+    ]
+}
+    $scope.openPdf = function() {
+        pdfMake.createPdf(dd).open();
+      };
+
+    $scope.downloadPdf = function() {
+        pdfMake.createPdf(docDefinition).download();
+      };
 });
