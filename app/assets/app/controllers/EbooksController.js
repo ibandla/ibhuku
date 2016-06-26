3ec84d61f7df4e9a686c82859fe6d5f38a4c024c
[anamikoye] <[anamikoye52@gmail.com]>
2016-06-26 20:57:44 +0300

    pdf Beginnings

--- app/assets/app/controllers/EbooksController.js
+++ app/assets/app/controllers/EbooksController.js
@@ -13,10 +13,10 @@
 
   $scope.setSelected = function(genre){
       $log.debug(genre);
-  
+
        $scope.selectedgenre = genre;
        $scope.genreparam = genre.id;
-     
+
   };
 
   $scope.ebooks = Ebook.all();
@@ -30,14 +30,60 @@
           { key: "ISBN", name: "ISBN", placeholder: "ISBN..." },
           { key: "author", name: "Author", placeholder: "Author..." },
           { key: "description", name: "Description", placeholder: "Description..." }
-        ]; 
-  
+        ];
+
   $scope.pageChangeHandler = function(num) {
       console.log('drinks page changed to ' + num);
   };
 
+  $scope.priceInfo = {
+    min: 0,
+    max: 1500
+  }
 
-  	
+  var docDefinition = {
+    content: [
+      {
+        text: 'IBHUKU CATALOG', style: 'header'
+      },
+      {
+        style: 'demoTable',
+        table: {
+          widths: ['*', '*', '*','*','*'],
+          body: [
+            [{text: 'Title', style: 'header'}, {text: 'Author', style: 'header'}, {text: 'Category', style: 'header'},
+              {text: 'ISBN', style: 'header'},{text: 'Price', style: 'header'}
+            ],
+            
+            ]
+        },
+        layout: 'lightHorizontalLines'
+      }
+    ],
+    styles: {
+      header: {
+        bold: true,
+        color: '#000',
+        fontSize: 11,
+        alignment: 'center'
+      },
+      demoTable: {
+        color: '#666',
+        fontSize: 10
+      }
+    }
+  };
+
+  $scope.openPdf = function() {
+      pdfMake.createPdf(docDefinition).open();
+    };
+
+  $scope.downloadPdf = function() {
+      pdfMake.createPdf(docDefinition).download();
+    };
+
+
+
 
     // $scope.searchParams = {
     //   "query":""
@@ -52,9 +98,4 @@
     //     ];
 
 
-    $scope.priceInfo = {
-      min: 0,
-      max: 1500
-    }
-    
-});
+    });
