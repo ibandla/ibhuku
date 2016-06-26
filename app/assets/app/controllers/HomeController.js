0c5cc528a027c6c505d820f38cdd9f1cf0d3b22a
Clive Makamara <cmosh@live.com>
2016-06-27 00:02:27 +0300

    Code Abstraction

--- app/assets/app/controllers/HomeController.js
+++ app/assets/app/controllers/HomeController.js
@@ -1,43 +1,10 @@
 app.controller ('HomeCtrl', function($scope,Ebook,$http,$auth,$rootScope,Restangular) {
 
-    var ebooks = Ebook.pdfdata();
-
-
-    var docDefinition = {
-      content: [
-        {
-          text: 'IBHUKU CATALOG', style: 'header'
-        },
-        {
-          style: 'demoTable',
-          table: {
-            widths: ['*', '*', '*','*','*'],
-            body: ebooks
-          },
-          layout: 'lightHorizontalLines'
-        }
-      ],
-      styles: {
-        header: {
-          bold: true,
-          color: '#000',
-          fontSize: 11,
-          alignment: 'center'
-        },
-        demoTable: {
-          color: '#666',
-          fontSize: 10
-        }
-      }
+    $scope.downloadPdf = function() {
+        Ebook.downloadPdf();
+    };
+    $scope.openPdf =  function() {
+        Ebook.openPdf();
     };
 
-
-
-    $scope.openPdf = function() {
-        pdfMake.createPdf(dd).open();
-      };
-
-    $scope.downloadPdf = function() {
-        pdfMake.createPdf(docDefinition).download();
-      };
 });
--- app/assets/app/factories/EbookFactory.js
+++ app/assets/app/factories/EbookFactory.js
@@ -7,21 +7,93 @@ app.factory('Ebook', function(Restangular) {
     function purchases() {
      return Restangular.all('purchases').getList().$object;
     }
-
     
     function one(id) {
      return Restangular.one('ebooks', id).get().$object;
     }
 
-    function pdfdata() {
-     return Restangular.all('pdfdata').getList().$object;
-    }
+    function downloadPdf() {
+
+        var basedata = Restangular.all('pdfdata');
+        basedata.getList().then(function(books) {
+        var docDefinition = {
+      content: [
+        {
+          text: 'IBHUKU CATALOG', style: 'header'
+        },
+        {
+          style: 'demoTable',
+          table: {
+            widths: ['*', '*', '*','*','*'],
+            body: books
+          },
+          layout: 'lightHorizontalLines'
+        }
+      ],
+      styles: {
+        header: {
+          bold: true,
+          color: '#000',
+          fontSize: 11,
+          alignment: 'center'
+        },
+        demoTable: {
+          color: '#666',
+          fontSize: 10
+        }
+      }
+    };
+
+        pdfMake.createPdf(docDefinition).download();
+
+        });
+  
+      }
+
+       function openPdf() {
+
+        var basedata = Restangular.all('pdfdata');
+        basedata.getList().then(function(books) {
+        var docDefinition = {
+      content: [
+        {
+          text: 'IBHUKU CATALOG', style: 'header'
+        },
+        {
+          style: 'demoTable',
+          table: {
+            widths: ['*', '*', '*','*','*'],
+            body: books
+          },
+          layout: 'lightHorizontalLines'
+        }
+      ],
+      styles: {
+        header: {
+          bold: true,
+          color: '#000',
+          fontSize: 11,
+          alignment: 'center'
+        },
+        demoTable: {
+          color: '#666',
+          fontSize: 10
+        }
+      }
+    };
+
+        pdfMake.createPdf(docDefinition).open();
+
+        });
+  
+      }
     
     return {
       all: all,
       one: one,
       purchases: purchases,
-      pdfdata: pdfdata
+      downloadPdf: downloadPdf,
+      openPdf: openPdf
     }
 
   });
