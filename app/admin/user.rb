78ff8f42e781c58089d361a5cfeb1f68a9d01ff5
ejakait <ejakaitcaleb@gmail.com>
2017-10-25 11:47:47 +0300

    small change

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register User do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-  before_filter :authorize, :except [:index]
+  before_filter :authorize, except: [:index]
 
   permit_params :blocked
 # or
